169837e4301caa04198a2235b34988772abcbb62
Sameh Kamal <sameh.kamaleldin@gmail.com>
2016-04-08 19:28:03 +0100

    add python install libs script

--- script/install-pyhton-libs.sh
+++ script/install-pyhton-libs.sh
@@ -12,3 +12,13 @@ pip3 install numpy
 pip3 install scipy
 pip3 install sklearn
 pip3 install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.1-cp35-none-any.whl
+pip3 install rope
+pip3 install jedi
+# flake8 for code checks
+pip3 install flake8
+# importmagic for automatic imports
+pip3 install importmagic
+# and autopep8 for automatic PEP8 formatting
+pip3 install autopep8
+# and yapf for code formatting
+pip3 install yapf
